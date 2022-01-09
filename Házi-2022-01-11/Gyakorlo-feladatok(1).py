7e829e69ba1b5b4d94e7ba8994ea028641f1eeaf
szasz.roland <szaszroland11@gmail.com>
2022-01-09 10:31:29 +0100

    commit

--- Házi-2022-01-11/Gyakorlo-feladatok(1).py
+++ Házi-2022-01-11/Gyakorlo-feladatok(1).py
@@ -1,58 +1,81 @@
-# # #1. feladat > Helló világ: Készíts programot, mely kiírja a képernyőre azt a szöveget, hogy „Hello World”!
-# # print("Hello World!")
+# ----importok----
+import math
+# ----------------
 
-# # #2. feladat >  Készíts programot, amely bekéri a felhasználó nevét, majd üdvözli őt a nevén szólítva!
-# # name = input("Add meg a neved> ")
-# # print("Üdvözöllek,", name )
+# # #1. feladat > Helló világ: Készíts programot, mely kiírja a képernyőre azt a szöveget, hogy „Hello World”!
+# print("Hello World!")
+
+
+# #2. feladat >  Készíts programot, amely bekéri a felhasználó nevét, majd üdvözli őt a nevén szólítva!
+# name = input("Add meg a neved> ")
+# print("Üdvözöllek,", name )
+
 
 # # #3. feladat > Készíts programot, mely bekéri a felhasználótól, hogy a kasszában hány 100, 200 és 500 Ft-os található. A program számolja ki, hogy mennyi a beírt pénz összege!
-# # szazasok = int(input("Add meg a Százasok számoát> "))
-# # ketszazasok = int(input("Add meg a Kétszázasok számoát> "))
-# # otszazasok = int(input("Add meg a Ötszázasok számoát> "))
+# szazasok = int(input("Add meg a Százasok számoát> "))
+# ketszazasok = int(input("Add meg a Kétszázasok számoát> "))
+# otszazasok = int(input("Add meg a Ötszázasok számoát> "))
 
-# # ertek100 = szazasok * 100
-# # ertek200 = ketszazasok * 200
-# # ertek500 = otszazasok * 500
+# ertek100 = szazasok * 100
+# ertek200 = ketszazasok * 200
+# ertek500 = otszazasok * 500
 
-# # result = ertek100 + ertek200 + ertek500
-# # print(result)
+# result = ertek100 + ertek200 + ertek500
+# print(result)
+
 
 # # # #4 feladat > Készíts programot, amely bekér a felhasználótól egy valós számot (Celsius fok), az eredményt átváltja Fahrenheit értékbe, és kiírja az eredményt a képernyőre (0°C=32°F, 40°C=104°F, lineáris)! Írd meg ugyanezt fordítva is!
 # # celsius = int(input("Add meg a celsius fokot> "))
 # # if celsius == 0:
 # #     print(32)
 # # else:
-# #     print((celsius * 1.8) + 32)
-    
+# #     print((celsius * 1.8) + 3
 
-# #5 feladat >  Készíts programot, mely két időpontot kérdez a felhasználótól (óra, perc, másodperc külön), majd kiszámítja a két időpont közötti időtartamot másodpercben, és az eredményt kiírja a képernyőre.
-# #-----------Eggyik óra-----------------------
-# ora1 = int(input("Hány óra?> "))
-# perc1 = int(input("Hány perc?> "))
-# masodperc1 = int(input("Hány másodperc?> "))
+#5 feladat >  Készíts programot, mely két időpontot kérdez a felhasználótól (óra, perc, másodperc külön), majd kiszámítja a két időpont közötti időtartamot másodpercben, és az eredményt kiírja a képernyőre.
+#-----------Eggyik óra-----------------------
+ora1 = int(input("Hány óra?> "))
+perc1 = int(input("Hány perc?> "))
+masodperc1 = int(input("Hány másodperc?> "))
 
-# OraInSec1 = (ora1 * 60) * 60
-# MinInSec1 = perc1 * 60
+OraInSec1 = (ora1 * 60) * 60
+MinInSec1 = perc1 * 60
 
-# result1 = OraInSec1 + MinInSec1 + masodperc1
-# #---------------------------------------------
+result1 = OraInSec1 + MinInSec1 + masodperc1
+#---------------------------------------------
 
-# #---------Másik óra---------------------------
-# ora2 = int(input("Hány óra?> "))
-# perc2 = int(input("Hány perc?> "))
-# masodperc2 = int(input("Hány másodperc?> "))
+#---------Másik óra---------------------------
+ora2 = int(input("Hány óra?> "))
+perc2 = int(input("Hány perc?> "))
+masodperc2 = int(input("Hány másodperc?> "))
 
-# OraInSec2 = (ora1 * 60) * 60
-# MinInSec2 = perc1 * 60
+OraInSec2 = (ora1 * 60) * 60
+MinInSec2 = perc1 * 60
 
-# result2 = OraInSec2 + MinInSec2 + masodperc2
-# #---------------------------------------------
+result2 = OraInSec2 + MinInSec2 + masodperc2
+#---------------------------------------------
 
-# #Nem tudom kiiratni mert nem fogadja el a else -t 
+if result1 > result2:
+    print(result1 - result2)
+else:
+    print(result2 - result1)
 
 
 # # 6 feladat >  Készíts programot, mely a felhasználótól bekért számról megállapítja, hogy az a.) pozitív, negatív vagy nulla, b.) egész vagy nem egész. Az eredményt a képernyőre szöveges válasz formájában írja ki!
 
+# num = float(input("Adj meg egy számot >"))
+
+# if num % 2 == 0:
+#     print("Páros")
+# elif num == 0:
+#     print("Ez nulla")
+# else:
+#     print("Páratlan")
+
+
+# if num % 1 == 0:
+#     print("Egész!")
+# else:
+#     print("Nem egész!")
 
 # #7 feladat > Készíts programot, mely beolvas a felhasználótól egy fizetést, és a fizetés nagyságától függően kiírja, hogy az alacsony, átlagos, vagy magas! A kategóriákat a saját preferenciád alapján határozhatod meg! :D
 
@@ -64,19 +87,47 @@
 #     print("átlagos")
 # elif fizetes > 270000:
 #     print("Magas")
-# # -------
+# # ----------------------
 
-#8 feladat  > Egy turkálóban minden póló darabja 500 Ft. Ha egy vásárlás során valaki több darabot is vesz, a második ára már csak 450 Ft, a harmadik pedig 400 Ft, de a negyedik és további darabok is ennyibe kerülnek, tehát az ár a harmadik vásárlása után már nem csökken tovább. Írj programot, amely a vásárolt pólók darabszámának ismeretében megmondja, hogy mennyit fizet a vásárló!
+# # 8 feladat  > Egy turkálóban minden póló darabja 500 Ft. Ha egy vásárlás során valaki több darabot is vesz, a második ára már csak 450 Ft, a harmadik pedig 400 Ft, de a negyedik és további darabok is ennyibe kerülnek, tehát az ár a harmadik vásárlása után már nem csökken tovább. Írj programot, amely a vásárolt pólók darabszámának ismeretében megmondja, hogy mennyit fizet a vásárló!
 
-# 11 feladat > Készíts programot, amely egy pozitív egész számról négyzetgyökvonás nélkül eldönti, hogy négyzetszám-e!
+# # 11 feladat > Készíts programot, amely egy pozitív egész számról négyzetgyökvonás nélkül eldönti, hogy négyzetszám-e!
 
-num = int(input("Adj meg egy pozitív egész számot > "))
+# num = int(input("Adj meg egy pozitív egész számot > "))
 
-if num % 2 == 0:
-    i = 0
-    while True:
-        i = num / 2
-        if i*i == num:
-            print("Ennek a számnak a gyöke:", i)
+# if num % 2 == 0:
+#     gyok = 0
+#     print(True)
+#     while True:
+#         gyok = num // 2
+#         power = math.pow(gyok, 2)
+#         if power == num:
+#             print("Ennek a számnak a gyöke:", gyok)
+# else:
+#     print(False)
 
-            #this is a test
+# # 12 feladat >Egy program bekér a felhasználótól két pozitív egész számot, és kiírja a két szám közötti összes páratlan számot. A program akkor is helyesen működik, ha a felhasználó előbb a felső, aztán az alsó határt adja meg (és fordítva is).  A program alogirtmusa a következő:
+# # -----------------------------------------------------------
+# # Első és második szám bekérése
+# # Ha első > második
+# #     alsó = második, felső = első
+# # Egyébként
+# #     alsó = első, felső = második
+# # A ciklusváltozó legyen alsó
+# # Amíg a ciklusváltozó kisebb vagy egyenlő a felsővel
+# #     Ha a ciklusváltozó páratlan
+# #        Írd ki a ciklusváltozót
+# #     Növeld a ciklusváltozót eggyel
+# # -----------------------------------------------------------
+
+# num1 = int(input("Adj meg egy pozitív egész számot > "))
+# num2 = int(input("Adj meg egy mésik pozitív egész számot > "))
+
+# if num1 > num2:
+#     lower = num1, upper = num2
+# else:
+#     lower = num2, upper = num1
+
+# helper = lower
+# if helper % 2 != 0:
+#     pass
